8589358110a3d43144b44a8031eb6b7bba5715dd
Vivek Maranganti <vivek-maranganti@Viveks-MacBook-Pro.local>
2026-05-30 16:29:59 -0500

    Parse HTTP request for method and path, select response based on path

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main() {
     //Create TCP socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
+    
     //Create and define the port address
     sockaddr_in addr{};
     addr.sin_family = AF_INET;
@@ -22,12 +23,35 @@ int main() {
 
     //Wait for, then process requests from the client
     listen(fd, 10);
+    //Accept loop
     while (true) {
+        //Accept client request and read buffer
         int client_fd = accept(fd, nullptr, nullptr);
         int buf_size = 1024;
         char buffer[buf_size];
         read(client_fd, buffer, buf_size);
-        string response = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
+
+        //Take first line to isolate method and path
+        string request(buffer);
+        string first_line = request.substr(0, request.find("\r\n"));
+
+        //Get the method
+        size_t methodEnd = first_line.find(" ");
+        string method = first_line.substr(0, methodEnd);
+
+        //Get the path
+        size_t pathStart = methodEnd+1;
+        size_t pathEndLen = first_line.find(" ", pathStart)-(pathStart);
+        string path = first_line.substr(pathStart, pathEndLen);
+
+        //Select response based on path
+        string response;
+        if (path == "/") {
+            response = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello";
+        } else {
+            response = "HTTP/1.1 404 Not Found\r\nContent-Length: 9\r\n\r\nNot Found";
+        }
+
         //Send HTTP response to client
         write(client_fd, response.c_str(), response.size());
         close(client_fd);
